1a5186107fd254a2d0a9c4000ef2cae6b62e0195
Dean <jasugun0000@gmail.com>
2018-01-22 10:07:31 +0800

    路徑錯誤修正

--- app/script/test.py
+++ app/script/test.py
@@ -10,13 +10,13 @@ import setting
 # 定義變數
 WEBDRIVER_PATH = os.environ.get('webdriver_path')
 DATA_PATH = os.environ.get('data_path')
-JSON_PATH = DATA_PATH + 'menu.json'
 NOVEL_NAME = '極道天魔'
-
 DATA_PATH = DATA_PATH + NOVEL_NAME + '/'
 if not os.path.exists(DATA_PATH):
     print('not exists so make new dir , and this is the path : ' + DATA_PATH)
     os.mkdir(DATA_PATH)
+
+JSON_PATH = DATA_PATH + 'menu.json'
 
 # 極道天魔目錄頁
 # TODO 未來應該建立一個 {小說: 目錄頁} mapping的json 專門給飄天文學網
