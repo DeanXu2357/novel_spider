eec75881df34dd5de2dde64076705e97253b7d44
Dean <jasugun0000@gmail.com>
2018-01-23 11:58:29 +0800

    可以正確獲取文章內容

--- app/script/test2.py
+++ app/script/test2.py
@@ -1,9 +1,12 @@
+# 單頁爬取＆寫入檔案
 import os
 import time
 import re
 import json
+from pprint import pprint
 
 from selenium import webdriver
+import opencc
 
 import setting
 
@@ -18,16 +21,19 @@ if not os.path.exists(DATA_PATH):
 
 JSON_PATH = DATA_PATH + 'menu.json'
 
-# 極道天魔目錄頁
-# TODO 未來應該建立一個 {小說: 目錄頁} mapping的json 專門給飄天文學網
 DRIVER = webdriver.Chrome(WEBDRIVER_PATH)
 
 DRIVER.get('http://www.piaotian.com/html/8/8502/5379303.html')
 
 content = DRIVER.find_element_by_xpath(
-    '//*[@id="content"]').get_attribute('innerHTML')
+    '//*[@id="content"]').text
 
-print(content)
+# contents = DRIVER.find_elements_by_css_selector('#content > font')
+conv_content = opencc.convert(content, config='s2t.json')
+print(conv_content)
+# pprint(content)
+
+
+# print(content)
 
 DRIVER.close()
-
